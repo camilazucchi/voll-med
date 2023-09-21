605bad6655e36253a0b0efdafec2c2c70c841b43
Camila Zucchi <camilazucchi@outlook.com>
2023-09-21 10:48:33 -0300

    refactor: improve handleResourceNotFoundException method

--- src/main/java/med/voll/api/exceptions/MedVollExceptions.java
+++ src/main/java/med/voll/api/exceptions/MedVollExceptions.java
@@ -1,6 +1,7 @@
 package med.voll.api.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,8 +12,8 @@ public class MedVollExceptions {
     @ExceptionHandler(EntityNotFoundException.class)
     // Essa anotação é usada em um método de um controlador para indicar que o método deve ser invocado quando uma
     // exceção do tipo "EntityNotFoundException" é lançada durante o processamento de uma requisição HTTP.
-    public void handleResourceNotFoundException() {
-
+    public ResponseEntity<String> handleResourceNotFoundException() {
+        return ResponseEntity.notFound().build();
     }
 
 }
